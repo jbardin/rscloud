b102181294951cfc5087bfe7c44651db5a255c38
James Bardin <jbardin@litl.com>
2012-10-09 10:06:10 -0400

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+setup(
+        name = 'rscloud',
+        version = '0.1',
+        author = 'James Bardin',
+        author_email = 'jbardin@litl.com',
+        license = 'MIT',
+        url = 'https://github.com/jbardin/rscloud.git',
+        packages = ['rscloud'],
+        install_requires = ['requests'],
+)
