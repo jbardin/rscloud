c02367ef736897bffd6a5ae2c51e1dee56ac38c7
James Bardin <jbardin@litl.com>
2012-10-09 15:10:33 -0400

    Domain function
    Added basic domain functions. Callbacks still need to be handled
    manually.

--- rscloud/domains.py
+++ rscloud/domains.py
@@ -8,26 +8,26 @@ class Domains(object):
         """
         Rackspace Domains API
 
-        :param session: authenticated requests session
+        :param session: rscloud.AuthenticatedSession
         """
         self._sess = session
-        self.url = session.sc['cloudDNS']['publicURL']
+        self._url = session.sc['cloudDNS']['publicURL'] + '/domains'
 
     def list(self, domain_name=None):
         if domain_name:
             params = {'name': domain_name}
         else:
             params = None
-        resp = self._sess.get(self.url, params=params)
+        resp = self._sess.get(self._url, params=params)
         return resp.json
 
     def list_subdomains(self, domain_id):
-        url = self.url + '/' + str(domain_id) + '/subdomains'
+        url = self._url + '/' + str(domain_id) + '/subdomains'
         resp = self._sess.get(url)
         return resp.json
 
     def detail(self, domain_id, records=True, subdomains=False):
-        url = self.url + '/' + str(domain_id)
+        url = self._url + '/' + str(domain_id)
         params = {'showRecords': str(bool(records)).lower(),
                     'showSubdomains': str(bool(subdomains)).lower()
                     }
@@ -36,15 +36,16 @@ class Domains(object):
         return resp.json
 
     def changes(self, domain_id, since=None):
-        url = self.url + '/' + str(domain_id) + '/changes'
+        url = self._url + '/' + str(domain_id) + '/changes'
+        params = {}
         if since:
-            params = {'since': since.isoformat()}
+            params['since'] = since
 
         resp = self._sess.get(url, params=params)
         return resp.json
 
     def export(self, domain_id):
-        url = self.url + '/' + str(domain_id) + '/export'
+        url = self._url + '/' + str(domain_id) + '/export'
         resp = self._sess.get(url)
         return resp.json
 
@@ -52,12 +53,12 @@ class Domains(object):
         # async with callback
         # TODO: make it easier to specify domain records
         body = json.dumps(domain_records)
-        resp = self._sess.post(self.url, data=body)
+        resp = self._sess.post(self._url, data=body)
         return resp.json
 
     def import_domains(self, records):
         # async with callback
-        url = self.url + '/import'
+        url = self._url + '/import'
         # contents must be bind_9 records
         if isinstance(records, basestring):
             records = [records]
@@ -73,7 +74,7 @@ class Domains(object):
 
     def modify(self, domain_id, email, ttl, comment):
         # async with callback
-        url = self.url + '/' + str(domain_id)
+        url = self._url + '/' + str(domain_id)
         body = {'comment': comment,
                 'emailAddress': email,
                 'ttl': int(ttl),
@@ -87,9 +88,9 @@ class Domains(object):
         assert isinstance(domain_ids, (list, tuple))
         params = {}
         if len(domain_ids) == 1:
-            url = self.url + '/' + str(domain_ids[0])
+            url = self._url + '/' + str(domain_ids[0])
         else:
-            url = self.url
+            url = self._url
             params['id'] = domain_ids
 
         if subdomains:
@@ -102,13 +103,70 @@ class Domains(object):
 class Records(object):
     def __init__(self, session):
         self._sess = session
-        self.url = session.sc['cloudDNS']['publicURL']
+        self._url = session.sc['cloudDNS']['publicURL'] + '/domains'
 
-    def get_record_from_name(self, name):
-        pass
+    def list(self, domainId):
+        url = self._url + '/' + str(domainId) + '/records'
+        resp = self._sess.get(url)
+        return resp.json
 
-    def list(self, domain_id):
-        pass
+    def search(self, domainId, record_type=None, name=None, data=None):
+        url = self._url + '/' + str(domainId) + '/records'
+        params = {
+            'type': record_type,
+            'name': name,
+            'data': data,
+        }
+        resp = self._sess.get(url, params=params)
+        return resp.json
+
+    def detail(self, domainId, recordId):
+        url = self._url + '/' + str(domainId) + '/records/' + str(recordId)
+        resp = self._sess.get(url)
+        return resp.json
+
+    def add(self, domainId, name, data, record_type, priority=None,
+            ttl=None, comment=None):
+        #TODO: add multiple records
+        url = self._url + '/' + str(domainId) + '/records'
+        record = {
+            'name': name,
+            'type': record_type,
+            'data': data,
+        }
+        if priority:
+            record['priority'] = priority
+        if ttl:
+            record['ttl'] = ttl
+        if comment:
+            record['comment'] = comment
+
+        req_data = json.dumps({'records': [record]})
+
+        resp = self._sess.post(url, data=req_data)
+        return resp.json
+
+    def remove(self, domainId, recordId):
+        url = self._url + '/' + str(domainId) + '/records/' + str(recordId)
+        resp = self._sess.delete(url)
+        return resp.json
+
+    def modify(self, domainId, recordId, name=None, data=None,
+               priority=None, comment=None):
+        url = self._url + '/' + str(domainId) + '/records/' + str(recordId)
+        record_data = {}
+        if name:
+            req_data['name'] = name
+        if data:
+            req_data['data'] = data
+        if priority:
+            req_data['priority'] = priority
+        if comment:
+            req_data['comment'] = comment
+
+        req_data = json.dumps(record_data)
+        resp = self._sess.put(url, data=req_data)
+        return resp.json
 
 
 class Rdns(object):
