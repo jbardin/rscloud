bf30a4ad948eeb66e01fe3ef88bdd7691892581b
James Bardin <jbardin@litl.com>
2012-11-06 16:39:01 -0500

    Re-add OS_* env variables for backwards compat

--- rscloud/__init__.py
+++ rscloud/__init__.py
@@ -4,6 +4,7 @@
 """
 Rackspace API access library
 """
+import os
 
 from .session import AuthenticatedSession
 from .domains import Domains, Records
@@ -15,10 +16,28 @@ from .exceptions import RackspaceAuthError, RackspaceAPIError
 class RackspaceSession(object):
     def __init__(self, username=None, api_key=None, password=None,
                  region=None, auth_url=None):
-        self.username = username
-        self.api_key = api_key
+        if username:
+            self.username = username
+        elif 'OS_USERNAME' in os.environ:
+            self.username = os.environ['OS_USERNAME']
+        else:
+            raise RackspaceAuthError('username not defined')
+
+        if api_key:
+            self.api_key = api_key
+        elif 'OS_PASSWORD' in os.environ:
+            self.api_key = os.environ['OS_PASSWORD']
+        else:
+            raise RackspaceAuthError('api_key not defined')
+
+        if region:
+            self.region = region
+        elif 'OS_REGION_NAME' in os.environ:
+            self.region = os.environ['OS_REGION_NAME']
+        else:
+            self.region = None
+
         self.password = password
-        self.region = region
         self.auth_url = auth_url
         self.rs_session = AuthenticatedSession()
         self.authenticated = False
